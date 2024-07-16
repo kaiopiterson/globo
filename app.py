5d92bf7c574d953c156ad78a08e877fd5f68007e
kaiopiterson <kaiopiterson@gmail.com>
2024-07-16 01:34:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
-# app.py
+import logging
 from flask import Flask
-from api import api_bp
+from config import Config
+from api import api_bp, db
+
+logging.basicConfig(level=logging.DEBUG)
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+
+    logging.debug('Initializing SQLAlchemy')
+    db.init_app(app)
+
+    logging.debug('Registering Blueprints')
     app.register_blueprint(api_bp)
+
     return app
 
 if __name__ == '__main__':
+    logging.debug('Starting application')
     app = create_app()
+    with app.app_context():
+        logging.debug('Creating database tables')
+        db.create_all()
     app.run(host='0.0.0.0', port=5000)
-
